747c953fddf29c2bbb67c2eeb7c350a9897fd4ef
slim abid <slimabid10@gmail.com>
2021-06-12 17:37:52 +0100

    resoring Home

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 /*import MusicPlayer from './components/MusicPlayer';*/
 import NavigationBar from './components/NavigationBar';
 import ControlClima from './components/ControlClima';
-import MusicPlayer from './components/MusicPlayer'
+import MusicPlayer from './components/MusicPlayer';
+import Home from './components/Home.js'
 import Global from './components/Global'
 import './index.css';
 ReactDOM.render(
-    <Global/>,
+    <Home/>,
   document.getElementById('root')
 );
