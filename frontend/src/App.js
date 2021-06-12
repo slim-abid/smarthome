1580153c6676e2ca1169f63b84b640b6e971c02f
Ines-chihi3 <ines.chihi.iia4@gmail.com>
2021-06-12 09:12:59 +0200

    second type of notification block

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,46 @@ import Dashboard from "./components/Dashboard.component"
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import React from 'react';
+import './App.css';
+import NavigationBar from './NavigationBar';
+import { BrowserRouter as Router , Route ,Switch } from 'react-router-dom' ;
+import Home from './home'
+import Dashboard from './dashboard'
+import Energy from './energy'
+import Streaming from './streaming'
 
 function App() {
   return (
     <Router>
-      <div className="container">
-      
-      <Switch>
-        <Route path='/login' exact component={Login}/>
-        <Route path='/signup' exact component={Signup}/>
-        <Route path='/' exact component={Dashboard}>
-          
-        </Route>
-       
-      </Switch>
+      <div className="App">
+        <NavigationBar />
+        <div className='content'>
+          <Switch>
+            <Route exact path='/home'>
+              <Home />
+            </Route>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/energy'>
+              <Energy />
+            </Route>
+            <Route exact path='/streaming'>
+              <Streaming />
+            </Route>
+            <Route path='/login' exact component={Login}/>
+            <Route path='/signup' exact component={Signup}/>
+            <Route path='/' exact component={Dashboard}>
+          </Switch>
+        </div>
       </div>
-    </Router>);
+    </Router>
+  );
 }
 
 export default App;
+
+
+
+
