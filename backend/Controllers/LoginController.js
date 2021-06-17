39ecb38a41875798b0de3778812ff99973651d0a
slim abid <slimabid10@gmail.com>
2021-06-17 04:29:33 +0100

    still needs some auth changes

--- backend/Controllers/LoginController.js
+++ backend/Controllers/LoginController.js
@@ -1,7 +1,16 @@
   
 const {request}=require('express')
 const User=require('../Models/UserModel')
+const cookieParser = require('cookie-parser');
+const cors = require("cors");
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 module.exports.login_get=(request,response)=>{    User.find()
     .then(users=>response.json(users))
     .catch(console.log("No users"))}
@@ -14,7 +23,7 @@ module.exports.login_post=async(request,response)=>
         const user=await User.login(email,password);
         response.send(user._id)
         console.log(user,'connected');
-       
+        res.cookie('auth', user._id, { httpOnly: true }).sendStatus(200)
         /*response.sendStatus(200)
         console.log('connected');*/
     } catch (error) {response.status(404).send(error)}
